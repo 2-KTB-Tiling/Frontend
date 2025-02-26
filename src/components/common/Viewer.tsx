236eb12ee24d85de9885685bc0150b9f54088955
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-02-26 21:27:00 +0900

    feat: 마크다운 뷰어 생성

---
+++ src/components/common/Viewer.tsx
@@ -0,0 +1,40 @@
+import Markdown from "react-markdown";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import remarkGfm from "remark-gfm";
+
+type ViewerType = {
+  content: string;
+};
+
+export default function Viewer({ content }: ViewerType) {
+  return (
+    <section className="prose p-4 max-w-2xl w-full bg-chat-bg rounded-lg">
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code(props) {
+            const { ref, children, className, node, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                PreTag="div"
+                language={match[1]}
+                style={materialDark}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {content}
+      </Markdown>
+    </section>
+  );
+}
