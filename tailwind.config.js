bfd39bd9b052767349da747d1ed881df023ca584
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-02-26 14:24:54 +0900

    design: 주요 색상 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#4F46E5",
+        border: "#CBD5E1",
+        "chat-bg": "#F8FAFC",
+        "dark-text": "#475569",
+        "light-text": "#94A3B8",
+      },
+    },
   },
   plugins: [],
 };
