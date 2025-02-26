36c5de0c268498398f5ddf72ac307155f6c831ed
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-02-26 15:00:35 +0900

    feat: 로그인 페이지 디자인

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,19 @@
+import { FaGithub } from "react-icons/fa";
+import { PiNotebookBold } from "react-icons/pi";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <main className="flex flex-col justify-center items-center gap-20 h-full">
+      <header className="flex items-center gap-2 text-4xl">
+        <PiNotebookBold />
+        <h1 className="font-bold">TILing</h1>
+      </header>
+      <button className="flex items-center gap-4 px-8 md:px-12 py-2 md:py-3 border border-border rounded-full hover:bg-black hover:text-white transition-colors">
+        <FaGithub className="text-3xl" />
+        <span className="font-semibold text-sm md:text-base">
+          GitHub 로그인
+        </span>
+      </button>
+    </main>
+  );
 }
