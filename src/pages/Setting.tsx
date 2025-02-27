96c4f37298231985f1f98d22e402983115376ac3
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-02-27 16:04:21 +0900

    feat: 세팅 페이지 전송 버튼 생성

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -15,7 +15,7 @@ export default function Setting() {
     <main className="flex flex-col justify-center items-center w-full h-full">
       <Logo />
       <form className="mt-8" onSubmit={handleUrlSubmit}>
-        <div className="flex flex-col items-center gap-6">
+        <div className="flex flex-col items-center gap-6 relative">
           <label htmlFor="url" className="flex gap-4 text-lg font-bold">
             <FaGithub className="text-3xl" />
             GitHub 레포지토리 주소
@@ -25,9 +25,12 @@ export default function Setting() {
             type="text"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
-            className="border border-border rounded-lg outline-none px-4 py-2 w-96"
+            className="border border-border rounded-lg outline-none pl-4 pr-16 py-2 w-96"
             placeholder="GitHub 레포지토리 주소를 입력해주세요..."
           />
+          <button className="bg-accent w-12 h-8 text-white rounded absolute bottom-[5px] right-[6px] font-bold">
+            전송
+          </button>
         </div>
       </form>
     </main>
