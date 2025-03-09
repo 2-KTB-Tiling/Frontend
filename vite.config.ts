3fb702b4c9436afa95333c29c0c900a8af324cdb
luckyPrice <luckyprice1103@naver.com>
2025-03-09 15:55:24 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     allowedHosts: ["frontend"],  // ✅ frontend 컨테이너에서 접근 허용 (LLM 설정 영향 없음)
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        // target: "http://127.0.0.1:8000",
+        target: "http://llm:8000", //---> 로컬에서는 위의 코드로 컨테이너에서는 밑 코드로.
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
